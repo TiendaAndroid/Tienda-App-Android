6056148cb087326d97e86e35de168e20fb15ae3e
Lucio Reyes <A01378985@tec.mx>
2024-08-27 12:44:03 -0600

    Prueba 2
    Prueba 2

--- App/app/src/main/java/com/larc/appandroid/MainActivity.kt
+++ App/app/src/main/java/com/larc/appandroid/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello asasfafds$name!",
+        text = "$name!",
         modifier = modifier
     )
 }
