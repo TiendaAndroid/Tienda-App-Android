8389d208c2ab05d31994bbfd344992663b711aa4
Arturo BM <a01168331@tec.mx>
2024-09-17 15:12:17 -0600

    Lol
    lol

--- App/app/src/main/java/com/larc/appandroid/viewmodel/ProductoVM.kt
+++ App/app/src/main/java/com/larc/appandroid/viewmodel/ProductoVM.kt
@@ -15,6 +15,9 @@ class ProductoVM: ViewModel() {
     private val _listaTodosProductos = MutableStateFlow(listOf<Producto>())
     val estadoListaTodosProductos: StateFlow<List<Producto>> = _listaTodosProductos
 
+    private var beginning = true
+    private var ending = false
+
     // Interface para la vista
     fun getAllProductos() {
         viewModelScope.launch {
